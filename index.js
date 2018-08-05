5b59482d9430bb9fa209cb0f15e6babdd8d6f798
Quang Hiep <bacodekiller@gmail.com>
2018-08-05 23:44:48 +0700

    using router to manage project

--- index.js
+++ index.js
@@ -1,111 +1,20 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
+let movieRouter = require('./router/movierouter');
 
 app.use(bodyParser.json({
     type: 'application/json'
 }));
-
-let MovieStore = require('./moviestore.js');
-let movieStore = new MovieStore();
 
 // //get all movies
 // app.get('/movies', (req, res) => {
 //     return res.json(movieStore.all());
 // });
 
+app.use('/movies', movieRouter);
 app.get('/', (req, res) => {
     return res.redirect('/movies');
-});
-
-// get a movie
-app.get('/movies/:title', (req, res) => {
-
-    let foundMovies = movieStore.find(req.params.title);
-
-    if (foundMovies.length < 1) {
-        res.statusCode = 404;
-        return res.json({
-            message: 'movie not found'
-        });
-    }
-    return res.json({
-        message: 'found movie',
-        payload: foundMovies.pop()
-    });
-});
-
-// create new movie
-app.post('/movies', (req, res) => {
-    console.log(req.body);
-    // check input req input
-    if (!req.body.Title || req.body.Title.trim().length < 1) {
-        res.statusCode = 400;
-        return res.json({
-            message: 'missing or invalid title'
-        });
-    }
-    // check movie have exist
-    if (movieStore.has(req.body.Title)) {
-        res.statusCode = 400;
-        return res.json({
-            message: 'movie already existed'
-        });
-    }
-
-    movieStore.add(req.body);
-    return res.json({
-        message: 'movie added successfully'
-    });
-});
-
-// update movie info
-app.put('/movies/:title', (req, res) => {
-    if (!movieStore.update(req.params.title, req.body)) {
-        res.statusCode = 500; // Internal Server error
-        return res.json({
-            message: 'failed to update movie info'
-        });
-    }
-    return res.json({
-        message: 'update movie successfully'
-    });
-});
-
-// delete a movie
-app.delete('/movies/:title', (req, res) => {
-    if (!movieStore.has(req.params.title)) {
-        res.statusCode = 404
-        return res.json({
-            message: 'movie not found'
-        });
-    }
-    movieStore.remove(req.params.title);
-    return res.json({
-        message: 'delete movie successfully'
-    });
-});
-
-function paginate(data, size, page) {
-    let index = page - 1;
-    return data.slice(index * size, (index + 1) * size);
-}
-
-// search movie by title
-app.get('/movies', (req, res) => {
-    let movies = movieStore.search(req.query.title);
-
-    let page = parseInt(req.query.page) || 1,
-        size = parseInt(req.query.size) || 2;
-
-    let results = paginate(movies, size, page);
-    return res.json({
-        title: req.query.title,
-        totalPage: movies.length,
-        page: page,
-        size: size,
-        payload: results
-    });
 });
 
 app.listen(8000, () => {
